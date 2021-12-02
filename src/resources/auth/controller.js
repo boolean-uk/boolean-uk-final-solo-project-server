e5c296874b983f7b2dbddd46140fd50ef7c00b3a
ValentinNM <valentinm.web@gmail.com>
2021-12-02 12:47:20 +0200

    ADD: login logic and router + encryption

--- src/resources/auth/controller.js
+++ src/resources/auth/controller.js
@@ -1,7 +1,7 @@
 const prisma = require('../..//utils/dbClient')
 const bycript = require('bcrypt')
 const createToken = require('../../utils/authentification');
-const crypto = require('crypto')
+const crypto = require('crypto');
 
 const rounds = 9;
 
@@ -12,8 +12,6 @@ async function signup(req, res){
     const hashedPass = await bycript.hash(userPassword, rounds)
 
     const random_uname = crypto.randomBytes(15).toString('hex');
-    
-    console.log({userEmail, userPassword, hashedPass, random_uname})
 
     try {
 
@@ -27,7 +25,6 @@ async function signup(req, res){
 
         const token = createToken(user)
         
-        console.log({token})
         res.status(200).json({token})
     } catch (error) {
     
@@ -38,4 +35,54 @@ async function signup(req, res){
 
 }
 
-module.exports = {signup}
+async function login(req, res) {
+
+    const {email: inputEmail, password: inputPassword} = req.body
+
+    if(!inputEmail || !inputPassword) { 
+        res.status(400).json({ error: "Username or Password is missing\n Please verify the credentials have been added accordingly and try again!"})
+    }
+    
+    try {
+        
+        const user = await prisma.user.findUnique({ 
+            where : { 
+                email : inputEmail
+            }
+        })
+        
+        if(!user){ 
+            res.status(401).json({ error : "Authentification failed."})
+        }
+        
+        const match = await bycript.compare( inputPassword, user.password)
+
+        if (match) { 
+            const userToTokenise = {  
+                ...user
+            }
+
+            delete userToTokenise.password
+            delete userToTokenise.username
+
+            // userToTokenise.forEach((username, password) => {
+            //     delete userToTokenise[username, password]
+            // });
+
+            const token = createToken(userToTokenise)
+
+            res.status(201).json({token})
+        } else { 
+            res.status(401).json({ error : "Authentification failed."})
+        }
+
+    } catch (error) {
+        
+        console.error({error})
+
+        res.status(500).json({ error: error.message })
+    }
+    
+}
+
+module.exports = {signup, login}
